6b4fa5f296a07493dba3dcf2cb033cde00ec1aa3
ANKIT KUMAR <37244694+ankitk2703@users.noreply.github.com>
2020-11-27 13:22:39 +0530

    Remove firebase config

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -2,14 +2,11 @@ import firebase from 'firebase'
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDaHK9qoo3JUQjN3G4FxaTfg2BHTqcLwjM",
-    authDomain: "ecommerce-2afab.firebaseapp.com",
-    databaseURL: "https://ecommerce-2afab.firebaseio.com",
-    projectId: "ecommerce-2afab",
-    storageBucket: "ecommerce-2afab.appspot.com",
-    messagingSenderId: "711474757856",
-    appId: "1:711474757856:web:1711545c9de05ffedd5085",
-    measurementId: "G-QJN0PMLDLR"
+    <
+    
+    Enter you firebase config information
+    
+    >
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig)
